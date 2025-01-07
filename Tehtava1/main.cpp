aaf8a44deee050c938c4b4971ea2ce42321fac93
VolatileKoffee <veikka.lk.kemppainen@gmail.com>
2025-01-07 18:51:10 +0200

    First repo version

---
+++ Tehtava1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //input output library
+
+using namespace std; //jos tätä ei ole, niin "std::cout", "std::endl", jne.
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
