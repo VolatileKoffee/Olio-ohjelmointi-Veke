e273b863219d0188ca746138c9b892971a1f9ce6
VolatileKoffee <veikka.lk.kemppainen@gmail.com>
2025-01-09 14:02:30 +0200

    Tehtava1 ready

--- Tehtava1/main.cpp
+++ Tehtava1/main.cpp
@@ -1,8 +1,48 @@
-#include <iostream> //input output library
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+int game(int maxnum);
+using namespace std;
 
-using namespace std; //jos tätä ei ole, niin "std::cout", "std::endl", jne.
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(time(0)); //randomizing game seed for each round
+    int variableMaxnum = rand()%100+1; //variable maximum cap between 1-100
+    cout << "Guess a number between 1-" << variableMaxnum << "!" << endl;
+
+    int guessesInGame = game(variableMaxnum);
+    cout << "Total guesses: " << guessesInGame<< endl;
+
     return 0;
+}
+
+int game(int maxnum){
+
+    int secretNum = rand() % maxnum + 1;        //drawing a secret number between 0-maxnum and adding +1
+
+    //cout << "Debug:" << secretNum << endl; //FOR ERROR CHECKING
+
+    int gamestate = 0;                                //game ON or OFF
+    int playerGuess;                                  //a guess
+    int playerGuessesSum = 0;                         //total guesses of a player
+
+    while(gamestate == 0){
+        cin >> playerGuess;
+        playerGuessesSum++;
+
+        if (playerGuess == secretNum){                //right answer
+            cout << "Right guess!" << endl;
+            gamestate = 1;
+
+        }else{                                        //wrong answer
+            cout << "Wrong guess! ";
+
+            if (playerGuess > secretNum){                       //bigger than secret number
+                cout << "Hint: Guess was too high" << endl;
+            }else if(playerGuess < secretNum){                  //smaller than secret number
+                cout << "Hint: Guess was too low" << endl;
+            }
+        }
+    }
+    return playerGuessesSum;
 }
